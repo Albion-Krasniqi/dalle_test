22f9a45f2cb4bd8b5e80fe666be6245521adf802
Albion Krasniqi <55230142+Albion-Krasniqi@users.noreply.github.com>
2024-08-31 16:35:39 -0700

    pushingfiles

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,24 @@
+#!/bin/bash
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
 echo [$(date)]: "START"
-echo [$(date)]: "Creating conda env with python 3.9" # change py version as per your need
+
+# Check if conda is installed
+if ! command -v conda &> /dev/null
+then
+    echo "Conda could not be found. Please install Conda and try again."
+    exit
+fi
+
+echo [$(date)]: "Creating conda env with python 3.9"
 conda create --prefix ./env python=3.9 -y
-echo [$(date)]: "activate env"
-source activate ./env
-echo [$(date)]: "installing the requirements" 
+
+echo [$(date)]: "Activating the environment"
+# Use the correct activation command for conda on macOS
+source activate ./env || conda activate ./env
+
+echo [$(date)]: "Installing the requirements"
 pip install -r requirements.txt
-echo [$(date)]: "END" 
+
+echo [$(date)]: "END"
